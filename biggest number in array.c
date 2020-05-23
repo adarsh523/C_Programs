3325657a670a975b815a4efb66d62ab5938cc2d1
Adarsh Bhasker Srivastava <53910945+adarsh523@users.noreply.github.com>
2020-05-23 12:41:50 +0530

    Create biggest number in array.c

---
+++ biggest number in array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a[i]
+	printf("Enter no. of datas:")
+	scanf("%d",&b); 	
+	int max=0;
+	max=a[0];
+
+	for(i=0;i<b;i++)
+	{
+		printf("\n");
+		if(a[i]>max)
+		{
+			max=a[i];
+		}
+	}
+	printf("\n Biggest =%d",max);
+	return 0;
+}
